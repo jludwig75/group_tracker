09879921bcc6bfc02049dff2dad7f6e709494393
Jonathan <jr.ludwig@gmail.com>
2019-04-23 11:35:25 -0600

    Handle exceptions getting station location list

--- python/getlocs.py
+++ python/getlocs.py
@@ -3,10 +3,13 @@ import json
 
 def get_locations(server):
     result = []
-    conn = httplib.HTTPConnection(server)
-    conn.request("GET", "/locs")
-    response = conn.getresponse()
-    if response.status == 200 and response.reason == 'OK':
-        result = json.loads(response.read())
-    conn.close()
+    try:
+        conn = httplib.HTTPConnection(server)
+        conn.request("GET", "/locs")
+        response = conn.getresponse()
+        if response.status == 200 and response.reason == 'OK':
+            result = json.loads(response.read())
+        conn.close()
+    except:
+        pass
     return result
